5c8e9c68cdd73c7659ffa30be8f7c66f6a905b08
MUDASSIR ARIF <mudassir@dcaptech.org>
2025-02-03 15:58:58 +0500

    some changes

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         about: resolve(__dirname, 'about.html'),
-        contact: resolve(__dirname, 'contact.html')
+        contact: resolve(__dirname, 'contact.html'),
+        europe: resolve(__dirname, 'europe.html')
       }
     }
   }
